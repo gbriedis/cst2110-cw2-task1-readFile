f6d9b12fc98bab942021bb618fbe4071d2cbb910
Ginters <GB664@live.mdx.ac.uk>
2021-04-11 22:33:59 +0100

    Created all the required functions

--- src/Album.java
+++ src/Album.java
@@ -6,7 +6,7 @@ public class Album {
     private String artist;
     private String year;
     private String sales;
-    public ArrayList<String> albumArr = new ArrayList<>();
+    private ArrayList<String> tracks;
 
     public Album(String ranking, String title, String artist, String year, String sales) {
         this.ranking = ranking;
@@ -15,21 +15,16 @@ public class Album {
         this.year = year;
         this.sales = sales;
 
-        addContent(ranking, title, artist, year, sales);
+        tracks = new ArrayList<String>();
     }
 
 
-    public void addContent(String ranking, String title, String artist, String year, String sales) {
-        albumArr.add(ranking);
-        albumArr.add(title);
-        albumArr.add(artist);
-        albumArr.add(year);
-        albumArr.add(sales);
+    public String getRanking(){
+        return this.ranking;
     }
 
-
-    public String getTitle(){
-        return title;
+    public String getTitle() {
+        return this.title;
     }
 
     public String getArtist() {
@@ -42,6 +37,61 @@ public class Album {
 
     public String getSales() {
         return sales;
+    }
+
+    public void addTrack(String name, String length) {
+        tracks.add(name);
+        tracks.add(length);
+    }
+
+    public void outputTrack() {
+        String sTitle = "Title", sTime = "(Mins:Secs)";
+        System.out.println("Album title: " + this.title);
+        System.out.println("Artist: " + this.artist);
+        System.out.println("Year of release: " + this.year);
+        System.out.println("Sales to date: " + this.sales);
+        System.out.println("");
+        System.out.println("Track List:");
+        System.out.println("------------------------------");
+        System.out.println(sTitle+" | "+sTime);
+        System.out.println("------------------------------");
+
+        int count = 0;
+        for(int i = 0; i < tracks.size(); i++) {
+            if (count < 2) {
+                System.out.print(tracks.get(i));
+                count++;
+            }
+            else {
+                System.out.println("");
+                count = 0;
+                i--;
+            }
+        }
+        System.out.println("");
+        System.out.println("------------------------------");
+
+    }
+
+    public void findTrack(String phrase) {
+        for (int i=0;i<tracks.size();i++){
+            if(tracks.get(i).contains(phrase)){
+                System.out.println("Artist("+title+") Album ("+artist+")");
+                System.out.println("-------");
+                System.out.print(tracks.get(i));
+                i++;
+                System.out.println(tracks.get(i));
+                System.out.println("-------");
+
+                System.out.println("");
+            }
+        }
+    }
+
+
+    @Override
+    public String toString(){
+        return String.format("|%3s\t|%60s\t|%20s\t|%5s\t|%7s|", getRanking(), getTitle(), getArtist(), getYear(), getSales());
     }
 
 }
--- src/ApplicationRunner.java
+++ src/ApplicationRunner.java
@@ -1,18 +1,105 @@
+import sun.tools.java.ScannerInputReader;
+
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Scanner;
 
 
 public class ApplicationRunner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        Album[] albums = readFile();
 
+        while (true) {
+            outputChoices();
+            Scanner stream = new Scanner(System.in);
+            String userInput = stream.nextLine();
 
-        try {
-            ReadDatabase rdb = new ReadDatabase();
-        } catch (FileNotFoundException exception) {
-            exception.printStackTrace();
+            switch (userInput){
+                case "1":
+                    System.out.println("–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––");
+                    String ranking = "Rank", title = "Title", artist = "Artist", year = "Year", sales = "Sales";
+                    System.out.format("|%3s\t|%60s\t|%20s\t|%5s\t|%7s|", ranking, title, artist, year, sales);
+                    System.out.println("");
+                    System.out.println("–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––");
+                    outputAlbums(albums);
+                    System.out.println("–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––");
+                    break;
+                case "2":
+                    System.out.print("Enter album rank from list [0-19] :");
+                    int choice = stream.nextInt();
+                    albums[choice].outputTrack();
+                    break;
+                case "3":
+                    System.out.print("Enter search word or phrase > ");
+                    String trackInput = stream.nextLine();
+                    for (int i = 0; i < albums.length; i++) {
+                        albums[i].findTrack(trackInput);
+                    }
+                    break;
+                case "0":
+                    return;
+                default:
+                    System.out.println("Enter a valid choice");
+            }
         }
+    }
 
+    private static void outputChoices() {
+        System.out.println("List albums......1");
+        System.out.println("Select album.....2");
+        System.out.println("Search title.....3");
+        System.out.println("List albums......0");
+    }
 
+    private static void outputAlbums(Album[] albums) {
+        for(Album i: albums) {
+            System.out.println(i);
+        }
+    }
+
+    private static Album[] readFile() throws FileNotFoundException {
+        Album[] albums = new Album[20];
+
+        int numAlbums = 0, counter = 0;
+
+        String dataFile = System.getProperty("user.dir") + File.separator + "albums.txt";
+        File text = new File(dataFile);
+        Scanner scan = new Scanner(text);
+
+        while (scan.hasNextLine()) {
+            String record = scan.nextLine();
+            Scanner lineScan = new Scanner(record);
+            lineScan.useDelimiter(":");
+
+            String ranking, title, artist, year, sales, songName, songLength;
+
+            ranking = lineScan.next();
+            try {
+                int rankingInt = Integer.parseInt(ranking);
+                counter++;
+                if(counter == rankingInt) {
+                    title = lineScan.next();
+                    artist = lineScan.next();
+                    year = lineScan.next();
+                    sales = lineScan.nextLine();
+                    albums[numAlbums]=new Album(ranking, title, artist, year, sales);
+                    numAlbums++;
+                }
+            }
+            catch(NumberFormatException exception) {
+                Scanner songScan = new Scanner(record);
+                songScan.useDelimiter("[-\\(|\\)]");
+                songName = songScan.next();
+                songLength = songScan.nextLine();
+
+                if(songLength.equals("---------------------------------------------------------------------------------")){}
+                else {
+                    albums[numAlbums-1].addTrack(songName, songLength);
+                }
+            }
+        }
+        return albums;
     }
 }
